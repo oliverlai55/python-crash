808167b5e3cad6b0f396737862df89ab849e288d
oliverlai55 <oliverlai55@gmail.com>
2016-10-16 17:37:20 -0400

    Finish chapter 6 notes

--- ch6-dictionaries.py
+++ ch6-dictionaries.py
@@ -132,3 +132,26 @@ for name, languages in favorite_languages:
     print(name)
     for language in languages:
         print(language.title())
+
+
+# A Dictionary in a Dictionary
+users = {
+    'aeinstein': {
+        'first': 'albert',
+        'last': 'einstein',
+        'location': 'princeton',
+        },
+    'mcurie': {
+        'first': 'marie',
+        'last': 'curie',
+        'location': 'paris',
+        },
+    }
+
+for username, user_info in users.items():
+    print('\nUsername: ' + username)
+    full_name = user_info['first'] + ' ' + user_info['last']
+    location = user_info['location']
+
+    print('\tFull name: ' + full_name.title())
+    print('\tLocation: ' + location.title())
