b7644c871e7270562539692f16ea56a0da9bfd6c
oliverlai55 <oliverlai55@gmail.com>
2016-10-16 03:37:59 -0400

    Update chapter 4

--- ch4-working-lists.py
+++ ch4-working-lists.py
@@ -34,3 +34,43 @@ max(digits)
 sum(digits)
 
 squares = [value**2 for value in range(1,11)]
+
+# Exercises
+# 4-3
+for i in range(1,21):
+	print(i)
+
+# 4-6
+for i in range(1,21):
+	if(i % 2 != 0):
+		print(i)
+
+# 4-7
+cubes = [i**2 for i in range(3,31)]
+
+# Slice
+# a specfic group of itmes in a list
+print(digits[0:3])
+print(digits[:4])
+# starts at the beginning of the list
+
+print(digits[3:])
+# starts from 4th, till the end of the list
+
+# Copying a List via slice
+my_food = ['pizza', 'falafel', 'carro cake']
+frined_foods = my_foods[:]
+
+print("my fav foods are")
+print(my_foods)
+
+# Tuples
+# immutalbe list is called a tuple
+dimensions = (200, 50)
+print(dimensions[0])
+
+for dimension in dimensions:
+	print(dimension)
+
+# you can rewrite a Tuples
+dimensions = (400, 100)
